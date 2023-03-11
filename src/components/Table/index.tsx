6dba00f42d8877ca84d79d6253ff72b7954f7e81
syedmahmad <devm.ahmad@gmail.com>
2023-03-11 12:02:53 +0500

    added table ui

---
+++ src/components/Table/index.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import { 
+  Box, Table, TableBody, TableCell, TableContainer,
+  TableRow, Paper, Card, CardHeader, Button, TableHead,
+} from '@mui/material';
+import { MoreVert } from '@mui/icons-material';
+
+interface TablePaginationActionsProps {
+  count: number;
+  page: number;
+  rowsPerPage: number;
+  onPageChange: (
+    event: React.MouseEvent<HTMLButtonElement>,
+    newPage: number,
+  ) => void;
+}
+
+interface TableProps {
+    handleOpen: () => void;
+    tableData: any;
+}
+
+export default function CustomPaginationActionsTable({ handleOpen, tableData } : TableProps) {
+  return (
+        <Card sx={{ borderRadius: '16px' }}>
+            <CardHeader title="Orders" action={<Button onClick={handleOpen} variant='contained' size='small' sx={{ borderRadius: '6px', padding: '12px 20px', background: '#4C6FFF', width: '120px', height: '36px', fontSize: '12px'}}>New Order</Button>}/>
+            <TableContainer component={Paper} sx={{ borderRadius: 0 }}>
+                <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+                    <TableHead sx={{ background: '#FAFAFB', borderTop: '1px solid #EDF2F7', borderBottom: '1px solid #EDF2F7'}}>
+                        <TableRow>
+                            <TableCell sx={{ color: '#8492A6' }} align="left">TEAM MEMBER</TableCell>
+                            <TableCell sx={{ color: '#8492A6' }} align="left">PRIORITY</TableCell>
+                            <TableCell sx={{ color: '#8492A6' }} align="left">ORDER NUMBER</TableCell>
+                            <TableCell sx={{ color: '#8492A6' }} align="left">TEAM</TableCell>
+                            <TableCell sx={{ color: '#8492A6' }} align="left">DUE DATE</TableCell>
+                            <TableCell sx={{ color: '#8492A6' }} align="left"></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {tableData.map((item: any) => (
+                            <TableRow key={item.teamMember}>
+                                <TableCell align="left" sx={{ fontSize: '12px', fontWeight: 600, lineHeight: '20px'}}>{item.teamMember}</TableCell>
+                                <TableCell align="left" sx={{ fontSize: '14px', fontWeight: 400, lineHeight: '20px', color: '#425466'}}><Box sx={{ display: 'flex', alignItems: 'center'}}><Box sx={{ width: '10px', height: '10px', marginRight: '10px', background: `${item.priority === 'Low' ? '#F16063' : item.priority === 'Medium' ? '#F7936F' : '#66CB9F'}`, borderRadius: '50%'}}></Box>{item.priority}</Box></TableCell>
+                                <TableCell align="left" sx={{ fontSize: '14px', fontWeight: 400, lineHeight: '20px', color: '#425466'}}>{item.orderNumber}</TableCell>
+                                <TableCell align="left" sx={{ fontSize: '12px', fontWeight: 600, lineHeight: '20px', color: '#425466'}}>{item.team}</TableCell>
+                                <TableCell align="left" sx={{ fontSize: '14px', fontWeight: 400, lineHeight: '20px', color: '#425466'}}>{item.dueDate}</TableCell>
+                                <TableCell align="left" sx={{ display: 'flex', alignItems: 'center'}}><MoreVert sx={{ color: '#718096'}} /></TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Card>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,200 @@
+import React, { useEffect, useState } from "react";
+import Table from "../components/Table";
+import { Modal, Box, Typography, Button, TextField, Select, FormControl, InputLabel, MenuItem, FormHelperText } from "@mui/material";
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
+
+function createData(teamMember: string, priority: string, orderNumber: number, team: string, dueDate: string) {
+    return { teamMember, priority, orderNumber, team, dueDate };
+}
+
+const rows = [
+    createData('Robert Fox', 'High', 2345, 'Blue', '01/01/2024'),
+    createData('Darlene Robertson', 'Low', 210735, 'Red', '01/01/2024'),
+    createData('Theresa Webb', 'Medium', 7452342, 'Green', '01/01/2024'),
+    createData('Kristin Watson', 'Medium', 54234, 'Yellow', '01/01/2024'),
+    createData('Robert Fox', 'High', 2345, 'Blue', '01/01/2024'),
+    createData('Darlene Robertson', 'Low', 210735, 'Red', '01/01/2024'),
+];
+
+const Home: React.FC = () => {
+    const fields = {
+        name: '',
+        priority: '',
+        team_name: '',
+        date: '01/01/2024',
+        id: Math.floor(Math.random() * 10000)
+    };
+    const [data, setData] = React.useState(fields);
+    const [open, setOpen] = useState(false);
+    const [tableRows, setTableRows] = React.useState(rows);
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    useEffect(() => {
+        if(typeof window !== 'undefined')
+        {
+            const tableData = JSON.parse(localStorage.getItem('form_data')!) || [];
+            if( tableData.length > 0)
+            {
+                tableData.map((item: any) => {
+                    rows.push(
+                        createData(item.name, item.priority, item.id, item.team_name, item.date)
+                    );
+                    setTableRows(rows);
+                    return null
+                })
+            }
+        }
+    }, []);
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleChange = (e: any) => {
+        setData((prev) => {
+            return {
+              ...prev,
+              [e.target.name]: e.target.value,
+            };
+        });
+    }
+
+    const handleSave = () => {
+        if (typeof window !== undefined)
+        {   
+            const localData = JSON.parse(localStorage.getItem('form_data')!) || [];
+            if(localData?.length > 0)
+            {
+                localStorage.setItem('form_data', JSON.stringify([...localData, data]));
+            }else {
+                localStorage.setItem('form_data', JSON.stringify([data]));
+            }
+            setData(() => {
+              return {
+              ...fields,
+              id: Math.floor(Math.random() * 10000)
+            }});
+            const tableData = JSON.parse(localStorage.getItem('form_data')!) || [];
+            if( tableData.length > 0)
+            {
+                tableData.map((item: any) => {
+                    rows.push(
+                        createData(item.name, item.priority, item.id, item.team_name, item.date)
+                    );
+                    setTableRows(rows);
+                    return null
+                })
+            }
+        }
+    }
+
+    const style = {
+        position: 'absolute' as 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: {
+            sm: 510,
+            xs: '80%',
+        },
+        padding: '40px',
+        bgcolor: '#F7FAFC',
+        borderRadius: '16px',
+        boxShadow: 24,
+        p: 4,
+      };
+      
+
+    return(
+        <>
+            <Modal open={open} onClose={handleClose}>
+                <Box sx={style}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                        <Typography id="modal-modal-title" variant="h4" sx={{ fontSize: '28px', fontWeight: 600}}>
+                            Create a New Order
+                        </Typography>
+                        <br />
+                        <Typography id="modal-modal-description" sx={{ color: '#718096', fontSize: '14px', fontWeight: 400 }}>
+                            Fill out the required information to create a new order.
+                        </Typography>
+                    </Box>
+                    <Box sx={{ m: '30px 0px'}}>
+                        <Box sx={{width: '100%'}}>
+                            <Typography sx={{ color: '#425466', fontSize: '14px', fontWeight: 500, mb: 2}}>
+                                Team Member Name
+                            </Typography>   
+                            <TextField label="Placeholder" value={data?.name} name="name" onChange={handleChange} variant="filled" sx={{border: 0, boxShadow: '0px 1px 2px rgba(50, 50, 71, 0.08), 0px 0px 1px rgba(50, 50, 71, 0.2)', width: '100%', '& .MuiFilledInput-root': {
+                                background: '#fff',
+                            } }} />                      
+                        </Box>
+                        <Box sx={{width: '100%', mt: 4}}>
+                            <Typography sx={{ color: '#425466', fontSize: '14px', fontWeight: 500, mb: 2}}>
+                                Priority
+                            </Typography>   
+                            <FormControl fullWidth>
+                                <InputLabel>Placeholder</InputLabel>
+                                <Select
+                                    label="Age"
+                                    variant="filled"
+                                    sx={{'& .MuiSelect-filled': {
+                                        backgroundColor: '#fff !important',
+                                    }}}
+                                    value={data?.priority} 
+                                    name="priority" 
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value={'Low'}>Low</MenuItem>
+                                    <MenuItem value={'Medium'}>Medium</MenuItem>
+                                    <MenuItem value={'High'}>High</MenuItem>
+                                </Select>
+                            </FormControl>                     
+                        </Box>
+                        <Box sx={{width: '100%', mt: 4}}>
+                            <Typography sx={{ color: '#425466', fontSize: '14px', fontWeight: 500, mb: 2}}>
+                                Team
+                            </Typography>   
+                            <FormControl fullWidth>
+                                <InputLabel>Placeholder</InputLabel>
+                                <Select
+                                    label="Age"
+                                    variant="filled"
+                                    sx={{'& .MuiSelect-filled': {
+                                        backgroundColor: '#fff !important',
+                                    } }}
+                                    value={data?.team_name}
+                                    name="team_name"
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value={'Blue'}>Blue</MenuItem>
+                                    <MenuItem value={'Green'}>Green</MenuItem>
+                                    <MenuItem value={'Red'}>Red</MenuItem>
+                                    <MenuItem value={'Yellow'}>Yellow</MenuItem>
+                                </Select>
+                            </FormControl>                      
+                        </Box>
+                        <Box sx={{width: '100%', mt: 4}}>
+                            <Typography sx={{ color: '#425466', fontSize: '14px', fontWeight: 500, mb: 2}}>
+                                Due Date
+                            </Typography>   
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DatePicker sx={{width: '100%', backgroundColor: '#fff'}} />
+                            </LocalizationProvider>        
+                            <FormHelperText>Date format must be mm/dd/yyyyy</FormHelperText>      
+                        </Box>
+                    </Box>
+                    <Box sx={{display: 'flex', justifyContent: 'space-between', width: '100%'}}>
+                        <Button variant="contained" size='small' sx={{ borderRadius: '6px', padding: '12px 20px', background: '#E4ECF7', width: '83px', height: '36px', fontSize: '12px', color: '#505780', textTransform: 'unset'}}> Cancel </Button>
+                        <Button variant="contained" size='small' sx={{ borderRadius: '6px', padding: '12px 20px', background: '#4C6FFF', width: '83px', height: '36px', fontSize: '12px',  textTransform: 'unset'}} onClick={() => handleSave()}> Submit </Button>
+                    </Box>
+                </Box>
+            </Modal>
+            <Table handleOpen={handleOpen} tableData={tableRows}/>   
+        </>
+    )
+}
+
+export default Home;
